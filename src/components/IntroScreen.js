89dbeb7382f3860d6f76d6839d05702703320872
Gabriel Cid <gabrielcid8@gmail.com>
2022-04-18 01:51:13 -0400

    Fix intro screen

--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -15,7 +15,7 @@ export const IntroScreen = () => {
   return (
     <div style={{display: 'flex', flexDirection: 'column' ,justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
       <MotionHoum transition={bounceTransition} animate={{y: ['20%', '-20%']}} />
-      <div>
+      <div style={{textAlign: 'center'}}>
           <h1 style={{color: '#263238'}}>¡Preparando tu nueva experiencia!</h1>
       </div>
     </div>
